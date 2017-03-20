9c9259a3ac77159beae3fae7ec544708c417c458
Michael Stepanov <michael@stepanoff.org>
2017-03-20 12:58:15 +0200

    Baked model for languages

---
+++ src/Model/Table/LanguagesTable.php
@@ -0,0 +1,89 @@
+<?php
+namespace Translations\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Languages Model
+ *
+ * @property \Cake\ORM\Association\HasMany $Translations
+ *
+ * @method \Translations\Model\Entity\Language get($primaryKey, $options = [])
+ * @method \Translations\Model\Entity\Language newEntity($data = null, array $options = [])
+ * @method \Translations\Model\Entity\Language[] newEntities(array $data, array $options = [])
+ * @method \Translations\Model\Entity\Language|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \Translations\Model\Entity\Language patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \Translations\Model\Entity\Language[] patchEntities($entities, array $data, array $options = [])
+ * @method \Translations\Model\Entity\Language findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class LanguagesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('languages');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->hasMany('Translations', [
+            'foreignKey' => 'language_id',
+            'className' => 'Translations.Translations'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->uuid('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name')
+            ->add('name', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
+
+        $validator
+            ->requirePresence('short_code', 'create')
+            ->notEmpty('short_code');
+
+        $validator
+            ->allowEmpty('description');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['name']));
+
+        return $rules;
+    }
+}
