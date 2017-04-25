dd79b5c8462280c5dd1e518fe293b02a9bf44288
Michael Stepanov <michael@stepanoff.org>
2017-04-25 18:22:04 +0300

    Fixed tests (task #806)

--- tests/Fixture/LanguagesFixture.php
+++ tests/Fixture/LanguagesFixture.php
@@ -18,14 +18,15 @@ class LanguagesFixture extends TestFixture
     // @codingStandardsIgnoreStart
     public $fields = [
         'id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        'name' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'short_code' => ['type' => 'string', 'length' => 6, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'code' => ['type' => 'string', 'length' => 6, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
         'description' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'is_rtl' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => false, 'comment' => '', 'precision' => null],
+        'is_active' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => true, 'comment' => '', 'precision' => null],
         'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
-            'UNIQUE_NAME' => ['type' => 'unique', 'columns' => ['name'], 'length' => []],
+            'UNIQUE_NAME' => ['type' => 'unique', 'columns' => ['code'], 'length' => []],
         ],
         '_options' => [
             'engine' => 'InnoDB',
@@ -42,24 +43,21 @@ class LanguagesFixture extends TestFixture
     public $records = [
         [
             'id' => '00000000-0000-0000-0000-000000000001',
-            'name' => 'Russian',
-            'short_code' => 'ru',
+            'code' => 'ru',
             'description' => 'Russian Language',
             'created' => '2017-03-20 12:55:32',
             'modified' => '2017-03-20 12:55:32'
         ],
         [
             'id' => '00000000-0000-0000-0000-000000000002',
-            'name' => 'German',
-            'short_code' => 'de',
+            'code' => 'de',
             'description' => 'German Language',
             'created' => '2017-03-30 12:55:32',
             'modified' => '2017-03-30 12:55:32'
         ],
         [
             'id' => '00000000-0000-0000-0000-000000000003',
-            'name' => 'Chinese',
-            'short_code' => 'cn',
+            'code' => 'cn',
             'description' => 'Chinese Language',
             'created' => '2017-03-30 12:55:32',
             'modified' => '2017-03-30 12:55:32'
