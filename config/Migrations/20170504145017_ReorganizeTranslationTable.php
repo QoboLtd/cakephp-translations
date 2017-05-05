cb05bc8abe048f50368e7d075c18f7e52c3934e4
Michael Stepanov <michael@stepanoff.org>
2017-05-05 12:35:51 +0300

    revert back language ID (task #806)

---
+++ config/Migrations/20170504145017_ReorganizeTranslationTable.php
@@ -0,0 +1,24 @@
+<?php
+use Migrations\AbstractMigration;
+
+class ReorganizeTranslationTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('translations');
+        $table->removeColumn('is_active');
+        $table->removeColumn('code');
+        $table->addColumn('language_id', 'string', [
+            'limit' => 36,
+            'null' => false,
+        ]);
+        $table->save();
+    }
+}
