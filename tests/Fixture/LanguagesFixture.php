bc50f907a55eca959825f8557ca6fbaf8137f2da
Michael Stepanov <michael@stepanoff.org>
2017-05-05 16:22:17 +0300

    Fixed test (task #806)

--- tests/Fixture/LanguagesFixture.php
+++ tests/Fixture/LanguagesFixture.php
@@ -19,7 +19,9 @@ class LanguagesFixture extends TestFixture
     public $fields = [
         'id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'code' => ['type' => 'string', 'length' => 6, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'name' => ['type' => 'string', 'length' => 50, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
         'is_rtl' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => false, 'comment' => '', 'precision' => null],
+        'trashed' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
         'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         '_constraints' => [
@@ -42,18 +44,21 @@ class LanguagesFixture extends TestFixture
         [
             'id' => '00000000-0000-0000-0000-000000000001',
             'code' => 'ru',
+            'name' => 'Russian',
             'created' => '2017-03-20 12:55:32',
             'modified' => '2017-03-20 12:55:32'
         ],
         [
             'id' => '00000000-0000-0000-0000-000000000002',
             'code' => 'de',
+            'name' => 'German',
             'created' => '2017-03-30 12:55:32',
             'modified' => '2017-03-30 12:55:32'
         ],
         [
             'id' => '00000000-0000-0000-0000-000000000003',
             'code' => 'cn',
+            'name' => 'Chinese',
             'created' => '2017-03-30 12:55:32',
             'modified' => '2017-03-30 12:55:32'
         ],
