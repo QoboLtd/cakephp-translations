bb46de4ccea998c080c05d162ac8a3fe362466bf
George Constantinou <georgec20001@gmail.com>
2017-11-06 17:14:35 +0200

    Moved API functionality to APP level (task #4795)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
-
 use Cake\Core\Configure;
-use Cake\Event\EventManager;
-use Translations\Event\Controller\Api\IndexActionListener;
 
 Configure::load('Translations.translations');
-
-EventManager::instance()->on(new IndexActionListener());
--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,10 @@
 <?php
-
 use Cake\Routing\Router;
 
 Router::plugin(
     'Translations',
     ['path' => '/language-translations'],
     function ($routes) {
-        $routes->prefix('api', function ($routes) {
-            /**
-             * handle json file extension on API calls
-             */
-            $routes->extensions(['json']);
-            $routes->resources('Translations');
-        });
         $routes->fallbacks('DashedRoute');
     }
 );
