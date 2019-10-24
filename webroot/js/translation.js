b396f7affc6d87efbc0f0eac8962c54f11b05e6d
George Constantinou <georgec20001@gmail.com>
2019-10-24 09:32:46 +0300

    Include CSRF token on request (task #5706)

--- webroot/js/translation.js
+++ webroot/js/translation.js
@@ -36,9 +36,16 @@ var translation = translation || {};
     $('button[name=btn_translation]').click(function () {
         lang = $(this).data('lang');
         formData = $('#form_translation_' + lang).serialize();
-        $.post('/language-translations/translations/addOrUpdate', formData, function (data) {
-            $('#result_' + lang).attr('class', data ? 'alert alert-success' : 'alert alert-danger');
-            $('#result_' + lang).html(data ? 'Translation is created or updated successfully.' : 'Translation cannot be saved. Please try later.').show().delay(5000).fadeOut();
+        $.post({
+            url: '/language-translations/translations/addOrUpdate',
+            headers: {
+                'X-CSRF-Token': localStorage.getItem('token_csrf')
+            },
+            data: formData,
+            success: function (data) {
+                $('#result_' + lang).attr('class', data ? 'alert alert-success' : 'alert alert-danger');
+                $('#result_' + lang).html(data ? 'Translation is created or updated successfully.' : 'Translation cannot be saved. Please try later.').show().delay(5000).fadeOut();
+            }
         });
     });
 })(jQuery);
